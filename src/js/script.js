783639e007c1295afb6308157232081dca360ca2
Eli-Jiron <elianguevara28@gmail.com>
2024-05-01 10:39:31 -0600

    Implementación de funcionalidad 4 (final)

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,12 @@ let contador = document.getElementById('contador');
 let like = document.getElementById('like');
 let dislike = document.getElementById('dislike');
 
+let correo = document.getElementById("correo");
+let comentario = document.getElementById("comentario");
+let enviar = document.getElementById("enviar");
+
+let valiCorreo = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
 like.addEventListener('click', function () {
     contador.innerHTML = parseInt(contador.innerHTML)+1
 });
@@ -11,3 +17,15 @@ dislike.addEventListener('click', function () {
         contador.innerHTML = parseInt(contador.innerHTML)-1
     }
 });
+
+enviar.addEventListener('click', function () {
+    if (correo.value == "" || comentario.value == "") {
+        alert('Debe llenar todos los campos')
+    } else {
+        if (!valiCorreo.test(correo.value)) {
+            alert('Ingrese un correo valido')  
+          } else {
+            alert('¡Se ha enviado su comentario!')
+          }
+    }
+})
